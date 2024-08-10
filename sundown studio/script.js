ffe62caae78715853c51e24a50c40ea63b875c95
Mahin-Hasan <mahinmtrs@gmail.com>
2024-08-10 21:33:01 +0600

    created featured products page with image popup on mouse enter and added animaiton

--- sundown studio/script.js
+++ sundown studio/script.js
@@ -3,3 +3,28 @@ const scroll = new LocomotiveScroll({
   smooth: true,
   //works only in no height or width is given to the wrapper div
 });
+
+var fixedImg = document.querySelector("#fixed-image");
+var ItemContainer = document.querySelector("#item-container");
+
+ItemContainer.addEventListener("mouseenter", function () {
+  fixedImg.style.display = "block";
+});
+ItemContainer.addEventListener("mouseleave", function () {
+  fixedImg.style.display = "none";
+});
+
+//beginner way
+// var item1 = document.querySelector("#item1");
+// item1.addEventListener("mouseenter", function () {
+//   var img = item1.getAttribute("data-image");
+//   fixedImg.style.backgroundImage= `url(${img})`
+// });
+//advanced way
+var items = document.querySelectorAll(".item");
+items.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixedImg.style.backgroundImage = `url(${image})`;
+  });
+});
