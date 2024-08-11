11594fb62d10a97b64ffbdb7f40a877579834c27
Mahin-Hasan <mahinmtrs@gmail.com>
2024-08-11 21:41:40 +0600

    added hamburger menu for small device, added animation and made home section responsive

--- sundown studio/script.js
+++ sundown studio/script.js
@@ -42,3 +42,19 @@ function swiperAnimation() {
 }
 swiperAnimation();
 page4Animation();
+
+var menu = document.querySelector("nav h3");
+var mobileNav = document.querySelector("#mobile-nav");
+var navImg = document.querySelector("nav img");
+var flag = 0;
+menu.addEventListener("click", function () {
+  if (flag === 0) {
+    mobileNav.style.top = 0;
+    navImg.style.opacity = 0;
+    flag = 1;
+  } else {
+    mobileNav.style.top = "-100%";
+    navImg.style.opacity = 1;
+    flag = 0;
+  }
+});
