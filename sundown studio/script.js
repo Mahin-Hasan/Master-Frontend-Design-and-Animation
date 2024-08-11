5d6c5f5c49480c12934fbee3786f5890f206f21c
Mahin-Hasan <mahinmtrs@gmail.com>
2024-08-12 02:11:35 +0600

    created loader animation and made overall layout responsive

--- sundown studio/script.js
+++ sundown studio/script.js
@@ -40,21 +40,29 @@ function swiperAnimation() {
     spaceBetween: 100,
   });
 }
+function menuAnimation() {
+  var menu = document.querySelector("nav h3");
+  var mobileNav = document.querySelector("#mobile-nav");
+  var navImg = document.querySelector("nav img");
+  var flag = 0;
+  menu.addEventListener("click", function () {
+    if (flag === 0) {
+      mobileNav.style.top = 0;
+      navImg.style.opacity = 0;
+      flag = 1;
+    } else {
+      mobileNav.style.top = "-100%";
+      navImg.style.opacity = 1;
+      flag = 0;
+    }
+  });
+}
 swiperAnimation();
 page4Animation();
+menuAnimation();
 
-var menu = document.querySelector("nav h3");
-var mobileNav = document.querySelector("#mobile-nav");
-var navImg = document.querySelector("nav img");
-var flag = 0;
-menu.addEventListener("click", function () {
-  if (flag === 0) {
-    mobileNav.style.top = 0;
-    navImg.style.opacity = 0;
-    flag = 1;
-  } else {
-    mobileNav.style.top = "-100%";
-    navImg.style.opacity = 1;
-    flag = 0;
-  }
-});
+
+var loader = document.querySelector('#loader')
+setTimeout(function(){
+  loader.style.top='-100%'
+},4200)
