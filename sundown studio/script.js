bfce190f6e6745f062f7d06b3eef957429315fdb
Mahin-Hasan <mahinmtrs@gmail.com>
2024-08-11 19:58:48 +0600

    created swiper container for scroll effect

--- sundown studio/script.js
+++ sundown studio/script.js
@@ -4,27 +4,41 @@ const scroll = new LocomotiveScroll({
   //works only in no height or width is given to the wrapper div
 });
 
-var fixedImg = document.querySelector("#fixed-image");
-var ItemContainer = document.querySelector("#item-container");
+function page4Animation() {
+  var fixedImg = document.querySelector("#fixed-image");
+  var ItemContainer = document.querySelector("#item-container");
 
-ItemContainer.addEventListener("mouseenter", function () {
-  fixedImg.style.display = "block";
-});
-ItemContainer.addEventListener("mouseleave", function () {
-  fixedImg.style.display = "none";
-});
-
-//beginner way
-// var item1 = document.querySelector("#item1");
-// item1.addEventListener("mouseenter", function () {
-//   var img = item1.getAttribute("data-image");
-//   fixedImg.style.backgroundImage= `url(${img})`
-// });
-//advanced way
-var items = document.querySelectorAll(".item");
-items.forEach(function (e) {
-  e.addEventListener("mouseenter", function () {
-    var image = e.getAttribute("data-image");
-    fixedImg.style.backgroundImage = `url(${image})`;
+  ItemContainer.addEventListener("mouseenter", function () {
+    fixedImg.style.display = "block";
   });
-});
+  ItemContainer.addEventListener("mouseleave", function () {
+    fixedImg.style.display = "none";
+  });
+
+  //beginner way
+  // var item1 = document.querySelector("#item1");
+  // item1.addEventListener("mouseenter", function () {
+  //   var img = item1.getAttribute("data-image");
+  //   fixedImg.style.backgroundImage= `url(${img})`
+  // });
+  //advanced way
+  var items = document.querySelectorAll(".item");
+  items.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      fixedImg.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+
+// Swiper
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 100,
+  });
+}
+swiperAnimation();
+page4Animation();
