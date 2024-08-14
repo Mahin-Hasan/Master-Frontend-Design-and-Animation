1f294e6743057b949e4cc8a4038bcb9c38f640c8
Mahin-Hasan <mahinmtrs@gmail.com>
2024-08-15 04:29:11 +0600

    fixed custom cursoer jitter issue and created a animated handburgaer menu

--- GSAP Animation Library/custom cursor GSAP/script.js
+++ GSAP Animation Library/custom cursor GSAP/script.js
@@ -1,10 +1,29 @@
-var main = document.querySelector("#main")
-var cursor = document.querySelector("#cursor")
+var main = document.querySelector("#main");
+var cursor = document.querySelector("#cursor");
+var imageDiv = document.querySelector("#image");
 
-main.addEventListener("mousemove",function(dets){
-    gsap.to(cursor,{
-        x: dets.x,
-        y: dets.y,
-        ease: "back.out",
-    })
-})
+main.addEventListener("mousemove", function (dets) {
+  gsap.to(cursor, {
+    x: dets.x,
+    y: dets.y,
+    duration: 0.6
+    // ease: "back.out",
+  });
+});
+imageDiv.addEventListener("mouseenter", function () {
+  cursor.innerHTML = "View More";
+  gsap.to(cursor, {
+    scale: 4,
+    backgroundColor: "#b5b5b57a "
+
+  });
+});
+imageDiv.addEventListener("mouseleave", function () {
+  cursor.innerHTML = "";
+
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundColor: "white "
+
+  });
+});
